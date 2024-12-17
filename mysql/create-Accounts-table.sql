34b2bb22e5067825de5614255d6d8bdbe423a279
sudha062000 <sudharani.lf185@gmail.com>
2024-12-17 01:52:24 -0600

    Create create-Accounts-table.sql

---
+++ mysql/create-Accounts-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Accounts (
+    account_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    site_name VARCHAR(255),
+    url VARCHAR(255),
+    password VARBINARY(255),
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
